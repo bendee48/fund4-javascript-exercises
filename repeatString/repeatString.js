d64fdc60aa38278a600811c6c972a7139333a4a1
Ben Daniels <benbanj@hotmail.com>
2019-03-22 13:49:37 +0000

    Initial commit

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+  let repeatedString = "";
+  for (let i = 0; i < number; i++) {
+    repeatedString += string;
+  }
+  if (number < 0) { repeatedString = "ERROR"}
+  return repeatedString;
 }
 
 module.exports = repeatString
