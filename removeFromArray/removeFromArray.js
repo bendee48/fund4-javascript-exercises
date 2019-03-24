63dfa09ce4957118e91cc1f08171de2154b2d4c6
Ben Daniels <benbanj@hotmail.com>
2019-03-24 17:30:37 +0000

    Completing remove from array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...items) {
+  for (let i = 0; i < items.length; i++) {
+    let ind = arr.indexOf(items[i]);
+    if (ind === -1) continue;
+    arr.splice(ind, 1);
+  }
+  return arr;
 }
 
 module.exports = removeFromArray
