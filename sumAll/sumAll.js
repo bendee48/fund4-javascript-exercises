0198e2776dc94dc9ab93c482296fdea0abb87470
Ben Daniels <benbanj@hotmail.com>
2019-03-24 18:16:38 +0000

    completing sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let sum_array = [];
+  let start = num1;
+  let end = num2;
 
+  if (num1 > num2){
+    start = num2;
+    end = num1;
+  }
+
+  if (num1 < 0 || num2 < 0) return "ERROR";
+  if (typeof num1 !== "number" || typeof num2 !== "number" ) return "ERROR";
+
+  for (start; start <= end; start++){
+    sum_array.push(start)
+  }
+  return sum_array.reduce((sum, item) => sum + item, 0);
 }
 
 module.exports = sumAll
