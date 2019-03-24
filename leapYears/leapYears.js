dc4b614bd9f014503621d9998cdfcd0ee1b83b3e
Ben Daniels <benbanj@hotmail.com>
2019-03-24 18:30:16 +0000

    completing leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  let result;
+  if (year % 400 === 0) {
+    result = true;
+  } else if (year % 100 === 0) {
+    result = false;
+  } else if (year % 4 === 0) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
 }
 
 module.exports = leapYears
