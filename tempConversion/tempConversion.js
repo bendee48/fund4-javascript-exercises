9a7e79718b60cc0e38afd5a1a45518405bb01be7
Ben Daniels <benbanj@hotmail.com>
2019-03-24 18:43:59 +0000

    Finishing tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let result = (temp - 32) * 5/9;
+  return +result.toFixed(1);
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let result = (temp * 9/5) + 32;
+  return +result.toFixed(1);
 }
 
 module.exports = {
