54537793b028d3b52aad9d70b82b12055b42a342
Ben Daniels <benbanj@hotmail.com>
2019-04-02 12:35:31 +0100

    Completing palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+  let newStr = str.toLowerCase();
+  newStr = newStr.replace(/[\s.,!]/g, "");
+  let reversedStr = newStr.split("").reverse().join("");
+  return newStr === reversedStr
 }
 
 module.exports = palindromes
