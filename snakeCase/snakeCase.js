ad0ccb5c0d1cd18dc9cee8eba6f21f65fc042f6f
Ben Daniels <benbanj@hotmail.com>
2019-04-02 13:01:49 +0100

    Completing snakeCase

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,9 @@
-const snakeCase = function() {
-
+const snakeCase = function(str) {
+  let words = str.match(/[a-z]+|[A-Z]{1}[a-zA-Z]+/g)
+  let snakeWords = words
+                      .map(item => item.toLowerCase())
+                      .join("_");
+  return snakeWords;
 }
 
 module.exports = snakeCase
