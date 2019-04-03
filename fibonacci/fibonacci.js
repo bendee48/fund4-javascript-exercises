61136dc0009100dc5db35b7f79ae0f10069e9d2d
Ben Daniels <benbanj@hotmail.com>
2019-04-03 16:35:16 +0100

    Completeting fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS"
+  num = +num;
+  const arr = [1,1];
+  for (i = 0; i < num-2; i++) {
+    let endOfArray = arr.slice(-2);
+    [num1, num2] = endOfArray;
+    let sum = num1 + num2;
+    arr.push(sum)
+  }
+  return arr.pop();
 }
 
 module.exports = fibonacci
