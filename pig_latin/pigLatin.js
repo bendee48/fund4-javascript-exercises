65f4f9d619689e2e559d3950a00594592b87e338
Ben Daniels <benbanj@hotmail.com>
2019-04-04 14:04:18 +0100

    Finishing pig latin

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,9 +1,18 @@
-function translate() {
-	// body...
+function translate(str) {
+	const words = str.split(" ");
+	let new_word = "";
+	for (i = 0; i < words.length; i++) {
+		if (/[aeiou]/.test(words[i][0])) new_word += words[i] + "ay ";
+		if (/[^aeiou]*qu/.test(words[i])) {
+			new_word += words[i].replace(/([^aieou]*qu)([aeiou]\w+)/, `$2$1` + "ay ")
+		} else if (/[^aioue]/.test(words[i][0])) {
+			new_word += words[i].replace(/([^aeiou]+)([aioue]\w*)/, `$2$1`) + "ay ";
+		}
+	}
+	return new_word.trim();
 }
 
 
 module.exports = {
 	translate
 }
-
